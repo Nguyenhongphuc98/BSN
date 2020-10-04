cbf2380f6e91ae10fe5f1e50ab64a5a5c97149d0
Phucnh <nguyenhongphuc98@gmail.com>
2020-10-04 09:51:37 +0700

    change to native ProgessView

--- BSN/Interface/Interface/Views/Components/Loading.swift
+++ BSN/Interface/Interface/Views/Components/Loading.swift
@@ -12,14 +12,17 @@ struct Loading: View {
     @State private var animate: Bool = false
     
     var body: some View {
-        Circle()
-            .trim(from: 0, to: 0.8)
-            .stroke(AngularGradient(gradient: .init(colors: [._primary, .white]), center: .center), style: StrokeStyle(lineWidth: 2, lineCap: .round))
-            .frame(width: 20, height: 20)
-            .rotationEffect(.init(radians: animate ? 360 : 0))
-            .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
-            .onAppear(perform: self.viewAppead)
-            .onDisappear(perform: self.viewDisAppead)
+//        Circle()
+//            .trim(from: 0, to: 0.8)
+//            .stroke(AngularGradient(gradient: .init(colors: [._primary, .white]), center: .center), style: StrokeStyle(lineWidth: 2, lineCap: .round))
+//            .frame(width: 20, height: 20)
+//            .rotationEffect(.init(radians: animate ? 360 : 0))
+//            .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+//            .onAppear(perform: self.viewAppead)
+//            .onDisappear(perform: self.viewDisAppead)
+        
+        ProgressView("Đang tải", value: 0, total: 1)
+            .progressViewStyle(CircularProgressViewStyle())
     }
     
     func viewAppead() {
